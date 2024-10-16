89545d0ee0cc6ae201881e8ae51976857506fde0
rendayigit <renda.yigit@gmail.com>
2024-10-16 19:47:03 +0300

    progress

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,9 @@ cd `dirname $0`
 SCRIPTDIR=`pwd`
 cd -
 
-# Run unit tests
+LD_LIBRARY_PATH=$SCRIPTDIR:$SCRIPTDIR/../bin/:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH
+
+# Run application
 cd $SCRIPTDIR/../bin/
 ./bc-gui
